6b81a5dd0e56ce159956e6d5570c3d42e9235167
jindrich-jedlicka <jedlicky.plzen@gmail.com>
2025-02-02 21:56:37 +0100

    time and satelites info

--- GpsDo/GpsDo.ino
+++ GpsDo/GpsDo.ino
@@ -22,8 +22,36 @@ RotaryEncoder *encoder;
 NeoSWSerial gpsSerial(PIN_RX, PIN_TX);
 
 static NMEAGPS nmeaGps;
-
 static int pos = 0;
+
+#define INVALID_VAL 0xFF
+
+//////////////////////////////////////////////////////////////////////////////
+// UTC Time
+//////////////////////////////////////////////////////////////////////////////
+
+#define TIME_LINE 0
+#define TIME_START_POS 6
+#define TIME_HOUR_POS TIME_START_POS + 2
+#define TIME_MINUTE_POS TIME_HOUR_POS + 3
+#define TIME_SEC_POS TIME_MINUTE_POS + 3
+
+static uint8_t _sec;
+static uint8_t _minute;
+static uint8_t _hour;
+
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// Satelites
+//////////////////////////////////////////////////////////////////////////////
+
+#define SAT_LINE 0
+#define SAT_START_POS 0
+
+static uint8_t _sat;
+
+//////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
@@ -39,7 +67,7 @@ void setup()
   PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);
 
   lcd.setCursor(0, 0);
-  lcd.print("Rotary Encoder");
+  //lcd.print("Rotary Encoder");
   encoder->tick(); // just call tick() to check the state.
 
   lcd.setCursor(0, 1);
@@ -49,6 +77,9 @@ void setup()
   Serial.begin(9600);
   Serial.println(" ");
   Serial.println("Neo-8M GPSDO ");
+
+  init_sat();
+  init_time();
 }
 
 ISR(PCINT0_vect)
@@ -83,15 +114,40 @@ void loop()
     lcd.setCursor(0, 1);
     lcd.print(pos, HEX);
   }
-  if (gpsSerial.available())
-  {
-    readChar = gpsSerial.read();
-    Serial.write(readChar);
-    //gpsParser.encode(readChar);
-  }
-  //nmeaGps.satellites[0];
-  //gpsParser.
 
+  while (nmeaGps.available(gpsSerial)) 
+  {
+    gps_fix fix = nmeaGps.read();
+    if (nmeaGps.satellites_valid())
+    {      
+      lcd.setCursor(8, 1);
+      lcd.print(nmeaGps.sat_count);
+
+      int tracked = 0;
+      for (uint8_t i = 0; i < nmeaGps.sat_count; i++)
+      {
+        if (nmeaGps.satellites[i].tracked)
+          tracked++;
+      }
+      lcd.setCursor(5, 1);
+      lcd.print(tracked);      
+
+      display_sat(fix.satellites);
+    }
+    if (fix.valid.time) 
+    {
+      display_hour(fix.dateTime.hours);
+      display_minute(fix.dateTime.minutes);
+      display_second(fix.dateTime.seconds);
+    }
+  }  
+//  if (gpsSerial.available())
+//  {
+//    readChar = gpsSerial.read();
+//    Serial.write(readChar);
+//    //gpsParser.encode(readChar);
+//  }
+  
   if (Serial.available() > 0)
   {
     readChar = Serial.read(); 
@@ -104,4 +160,67 @@ void loop()
 void checkPosition()
 {
   encoder->tick(); // just call tick() to check the state.
+}
+
+void init_sat()
+{
+  _sat = INVALID_VAL;
+  
+  lcd.setCursor(SAT_START_POS, SAT_LINE);    
+  lcd.print("S:  ");
+}
+
+void display_sat(uint8_t value)
+{
+  if (_sat != value)
+  {
+    display_number(value, SAT_LINE, SAT_START_POS + 2);
+    _sat = value;
+  }
+}
+
+void init_time()
+{
+  _sec = INVALID_VAL;
+  _minute = INVALID_VAL;
+  _hour = INVALID_VAL;  
+
+  lcd.setCursor(TIME_START_POS, TIME_LINE);    
+  lcd.print("T:  :  :  ");
+}
+
+void display_hour(uint8_t value)
+{
+  if (_hour != value)
+  {
+    display_number(value, TIME_LINE, TIME_HOUR_POS);    
+    _hour = value;
+  }
+}
+
+void display_minute(uint8_t value)
+{
+  if (_minute != value)
+  {
+    display_number(value, TIME_LINE, TIME_MINUTE_POS);    
+    _minute = value;
+  }
+}
+
+void display_second(uint8_t value)
+{
+  if (_sec != value)
+  {
+    display_number(value, TIME_LINE, TIME_SEC_POS);
+    _sec = value;
+  }
+}
+
+void display_number(uint8_t number, uint8_t line, uint8_t pos)
+{
+  lcd.setCursor(pos, line);      
+  if (number < 10)
+    lcd.print("0");
+    
+  lcd.print(number);
 }
