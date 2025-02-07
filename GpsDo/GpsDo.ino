f3c88bd3b026f195591ffdc8dc86ea01a8df6637
Tomas-Jedlicka <jedlicky.plzen@gmail.com>
2025-02-07 21:08:06 +0100

    Update GpsDo.ino

--- GpsDo/GpsDo.ino
+++ GpsDo/GpsDo.ino
@@ -22,11 +22,13 @@
 
 //--------------------
 
+#define MAX_INDEX NMEAGPS_MAX_SATELLITES + 1
+
 static GpsLiquidCrystal lcd;
 
 #define PIN_IN1 A3
 #define PIN_IN2 A2
-RotaryEncoder *encoder;
+static RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
 
 #define PIN_RX 2
 #define PIN_TX 3
@@ -34,6 +36,7 @@ static NeoSWSerial gpsSerial(PIN_RX, PIN_TX);
 
 static NMEAGPS nmeaGps;
 static int pos = 0;
+static int index = 0;
 
 static GpsView gpsView;
 static SateliteInfoView satView;
@@ -45,7 +48,6 @@ void setup()
   //  pinMode(start_pin, INPUT);
   //  pinMode(stop1_pin, INPUT);
   //  pinMode(reset_pin, INPUT);
-  encoder = new RotaryEncoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
 
   PCICR |= (1 << PCIE1);
   PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);
@@ -54,10 +56,7 @@ void setup()
   lcd.print("Neo-8M GPS DO ");
   lcd.setCursor(0, 1);
   lcd.print("Waiting for data");
-  encoder->tick(); // just call tick() to check the state.
-
-  lcd.setCursor(0, 1);
-  //lcd.print(pos, HEX);
+  encoder.tick(); 
 
   gpsSerial.begin(9600);
   Serial.begin(9600);
@@ -71,7 +70,7 @@ ISR(PCINT0_vect)
 ISR(PCINT1_vect)
 {
   NeoSWSerial::rxISR(PINC);
-  encoder->tick(); // just call tick() to check the state.
+  encoder.tick(); // just call tick() to check the state.
 }
 
 ISR(PCINT2_vect)
@@ -83,22 +82,23 @@ char readChar;
 
 void loop()
 {
-  //encoder->tick(); // just call tick() to check the state.
-
-  int newPos = encoder->getPosition();
-  if (newPos != pos)
-  {
-    pos = newPos;
-
-    // lcd.setCursor(0, 1);
-    // lcd.print("        ");
-    // lcd.setCursor(0, 1);
-    //lcd.print(pos, HEX);
-  }
-
+  update_index();
   while (nmeaGps.available(gpsSerial)) 
   {
-    gpsView.display_data(lcd, nmeaGps);
+    if (index == 0)
+    {
+      gpsView.display_data(lcd, nmeaGps);
+    }
+    else
+    {
+      const NMEAGPS::satellite_view_t *p_satelite_data = NULL;
+      uint8_t sat_index = (uint8_t)index - 1;
+
+      if (sat_index < nmeaGps.sat_count)
+        p_satelite_data = &nmeaGps.satellites[sat_index];
+
+      satView.display_data(lcd, index, p_satelite_data);
+    }
   }  
 //  if (gpsSerial.available())
 //  {
@@ -116,7 +116,25 @@ void loop()
   }
 }
 
-void checkPosition()
+void update_index()
 {
-  encoder->tick(); // just call tick() to check the state.
-}
+  int newPos = encoder.getPosition();
+  if (newPos != pos)
+  {    
+    int prevIndex = index;
+    index += newPos - pos;
+    if (index < 0)
+      index = 0;
+    else if (MAX_INDEX < index)
+      index = MAX_INDEX;
+
+    if (prevIndex != index)
+    {
+      if (index == 0)
+        gpsView.clear();
+      else if (prevIndex == 0)      
+        satView.clear();
+    }
+    pos = newPos;    
+  }  
+ }
